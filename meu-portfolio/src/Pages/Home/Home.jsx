cd4200706a4d920513fe697a6c20d8740ec1e52d
Indalecio Olivo <indalecioolivo@gmail.com>
2024-11-15 17:27:39 -0300

    feat: writting home page things

--- meu-portfolio/src/Pages/Home/Home.jsx
+++ meu-portfolio/src/Pages/Home/Home.jsx
@@ -1,3 +1,15 @@
+import "./Home.css";
+import Photo from "../../assets/photo-home.png";
+import ModelPhoto from "../../assets/model-photo.png";
 export default function Home() {
-  return <div>Page Home</div>;
+  return (
+    <section className="home-container">
+      <section>
+        <img src={Photo} alt="" />
+      </section>
+      <article>
+        <img src={ModelPhoto} alt="" />
+      </article>
+    </section>
+  );
 }
