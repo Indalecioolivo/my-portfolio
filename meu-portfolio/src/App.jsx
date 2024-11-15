42d3c38b820902560f20bd58b10fa57b26bcf866
Indalecio Olivo <indalecioolivo@gmail.com>
2024-11-15 14:11:45 -0300

    feat: routes created on main, header component finalized

--- meu-portfolio/src/App.jsx
+++ meu-portfolio/src/App.jsx
@@ -1,10 +1,12 @@
+import { Outlet } from "react-router-dom";
 import "./App.css";
+import Header from "./components/Header/Header";
 
 function App() {
   return (
     <>
-      <h1>Teste</h1>
-      <p>Olá Mundinho lindo</p>
+      <Header />
+      <Outlet />
     </>
   );
 }
--- meu-portfolio/src/main.jsx
+++ meu-portfolio/src/main.jsx
@@ -1,10 +1,35 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./index.css";
+import App from "./App.jsx";
+import Home from "./Pages/Home/Home.jsx";
+import Portfolio from "./Pages/Portfolio/Portfolio.jsx";
+import Contact from "./Pages/Contact/Contact.jsx";
 
-createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/portfolio",
+        element: <Portfolio />,
+      },
+      {
+        path: "/contact",
+        element: <Contact />,
+      },
+    ],
+  },
+]);
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <RouterProvider router={router} />
+  </StrictMode>
+);
