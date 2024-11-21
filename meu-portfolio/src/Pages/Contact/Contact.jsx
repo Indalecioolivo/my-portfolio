4e4b79bcbbed3d5f32f40db701be4c8feac0d926
Indalecio Olivo <indalecioolivo@gmail.com>
2024-11-21 14:48:16 -0300

    feat: formulário de contato implementado em pages/contact

--- meu-portfolio/src/Pages/Contact/Contact.jsx
+++ meu-portfolio/src/Pages/Contact/Contact.jsx
@@ -1,3 +1,29 @@
+import "./Contact.css";
 export default function Contact() {
-  return <div>Page Contact</div>;
+  return (
+    <section className="contact-container">
+      <article>
+        <h2>Entre em Contato</h2>
+        <p>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quas
+          possimus veniam quidem ipsum tempore minus ipsam animi, enim eaque
+          nostrum in temporibus explicabo dolor iusto illum, adipisci eveniet
+          expedita, est aspernatur quae. Hic tenetur delectus pariatur modi fuga
+          laboriosam commodi quasi error recusandae, asperiores dolores est
+          provident nobis temporibus nulla.
+        </p>
+      </article>
+      <article>
+        <h3>Contato</h3>
+        <form action="">
+          <label htmlFor="name">Nome</label>
+          <input type="text" id="name" />
+          <label htmlFor="email">E-mail</label>
+          <input type="text" id="email" />
+          <label htmlFor="textcontact">Nome</label>
+          <textarea name="" id="textcontact" disabled></textarea>
+        </form>
+      </article>
+    </section>
+  );
 }
